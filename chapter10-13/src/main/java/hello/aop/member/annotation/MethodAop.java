99a19813e1411606f369a672c4a202604f812be8
tevolx <tevolx0@gmail.com>
2024-12-21 23:15:47 +0900

    feat: add chapter11

--- chapter10-13/src/main/java/hello/aop/member/annotation/MethodAop.java
+++ chapter10-13/src/main/java/hello/aop/member/annotation/MethodAop.java
@@ -1,14 +1,12 @@
 package hello.aop.member.annotation;
 
-import lombok.extern.slf4j.Slf4j;
-
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.TYPE})
+@Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
-public @interface ClassAop {
-
+public @interface MethodAop {
+    String value();
 }
